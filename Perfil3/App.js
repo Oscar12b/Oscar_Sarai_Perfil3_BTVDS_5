dfd8d50b96045fc7645a44500b0f4d1aefd52979
smonterroza <20190288@ricaldone.edu.sv>
2024-05-21 00:41:27 -0600

    Ya esta el menú y la splash screen

--- Perfil3/App.js
+++ Perfil3/App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+// Define tus pantallas
+function Inicio() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Inicio</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Información() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Información</Text>
+    </View>
+  );
+}
+
+function Comidas() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Comidas favoritas</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Inicio') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Información') {
+              iconName = focused
+                ? 'school'
+                : 'school-outline';
+            } else if (route.name === 'Comidas') {
+              iconName = focused
+                ? 'fast-food'
+                : 'fast-food-outline';
+            }
+
+            // Puedes devolver cualquier componente aquí
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Inicio" component={Inicio} />
+        <Tab.Screen name="Información" component={Información} />
+        <Tab.Screen name="Comidas favoritas" component={Comidas} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
