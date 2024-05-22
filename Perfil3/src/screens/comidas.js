6f7a93f17670c801e6adb324e561574bcdcc679b
Oscar12b <106353456+PotCAR1452@users.noreply.github.com>
2024-05-21 23:51:25 -0600

    se añadiero imagenes

--- Perfil3/src/screens/comidas.js
+++ Perfil3/src/screens/comidas.js
@@ -3,14 +3,14 @@ import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
 
 function Comidas() {
     const comidasFavoritas = [
-        { id: '1', nombre: 'Pizza', descripcion: 'Deliciosa pizza con queso derretido y pepperoni' },
-        { id: '2', nombre: 'Sushi', descripcion: 'Variado sushi con arroz y pescado fresco' },
-        { id: '3', nombre: 'Tacos', descripcion: 'Tacos mexicanos con tortilla de maíz, carne y guacamole' },
-        { id: '4', nombre: 'Pasta', descripcion: 'Pasta italiana con salsa de tomate y albóndigas' },
-        { id: '5', nombre: 'Hamburguesa', descripcion: 'Hamburguesa jugosa con lechuga, tomate y queso cheddar' },
-        { id: '6', nombre: 'Pollo', descripcion: 'Pollo a la parrilla con verduras asadas' },
-        { id: '7', nombre: 'Sopa', descripcion: 'Sopa caliente con vegetales frescos y fideos' },
-        { id: '8', nombre: 'Ensalada', descripcion: 'Ensalada fresca con una variedad de verduras y aderezos' },
+        { id: '1', nombre: 'Pizza', descripcion: 'Deliciosa pizza con queso derretido y pepperoni', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7J6mSjH7-0HxAAg-MsxoCcr9YUF94FGAQpg&s' },
+        { id: '2', nombre: 'Sushi', descripcion: 'Variado sushi con arroz y pescado fresco', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaBaM7fMo7YNcCfIBQZVKSwdf_GOc3YMNq2g&s' },
+        { id: '3', nombre: 'Tacos', descripcion: 'Tacos mexicanos con tortilla de maíz, carne y guacamole', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzRSykTwcG150H249PAZ1mZiaincUKIMX18w&s' },
+        { id: '4', nombre: 'Pasta', descripcion: 'Pasta italiana con salsa de tomate y albóndigas', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IrgDep9jUgKjpBcy88WkFWMWNgntor_3og&s' },
+        { id: '5', nombre: 'Hamburguesa', descripcion: 'Hamburguesa jugosa con lechuga, tomate y queso cheddar', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA9oFe2yW9jYggHprCqfjoyxFz38rfLlYj3g&s' },
+        { id: '6', nombre: 'Pollo', descripcion: 'Pollo a la parrilla con verduras asadas', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwVLLxMiFQPAMVh_xcE2LEwwHozbW92z-7zg&s' },
+        { id: '7', nombre: 'Sopa', descripcion: 'Sopa caliente con vegetales frescos y fideos', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuGscTclibMH2TlX5JNZpGf27s1mPi0r_qVw&s' },
+        { id: '8', nombre: 'Ensalada', descripcion: 'Ensalada fresca con una variedad de verduras y aderezos', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrjNqoN34oDFMILijcYx2Y0n5xZYKOApKKbw&s' },
     ];
 
     return (
@@ -23,7 +23,7 @@ function Comidas() {
                 contentContainerStyle={styles.flatlistContainer}
                 renderItem={({ item }) => (
                     <View style={styles.card}>
-                        <Image source={item.imagen} style={styles.cardImage} />
+                        <Image source={{ uri: item.imagen }} style={styles.cardImage} />
                         <Text style={styles.cardText}>{item.nombre}</Text>
                         <Text style={styles.cardDescription}>{item.descripcion}</Text>
                     </View>
